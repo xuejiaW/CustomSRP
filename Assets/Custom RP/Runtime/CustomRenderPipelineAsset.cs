bd8173e4c2db72b0237ac0fe1b502aa9f08f22ba
XueJia Wang <wangxuejia@outlook.com>
2023-12-23 18:44:21 +0800

    feat: create custom render pipeline asset
    [Module]: Custom RP
    DetailInfo: 创建自定义渲染管线资源
    
    Signed-off-by: XueJia Wang <wangxuejia@outlook.com>

---
+++ Assets/Custom RP/Runtime/CustomRenderPipelineAsset.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[CreateAssetMenu(menuName = "Rendering/Custom Render Pipeline")]
+public class CustomRenderPipelineAsset : RenderPipelineAsset
+{
+    protected override RenderPipeline CreatePipeline() { return null; }
+}
