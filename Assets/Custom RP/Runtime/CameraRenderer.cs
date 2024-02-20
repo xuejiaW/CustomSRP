61d21a6999c1a0a7c96d90c762bb5b4103b440a3
XueJia Wang <wangxuejia@outlook.com>
2024-02-20 17:40:03 +0800

    feat: add command buffer to sample and clear render target and cull
    增加进行 Sample 和 Clear Render Target 的 Command Buffer，并进行了 Cull 操作
    
    Signed-off-by: XueJia Wang <wangxuejia@outlook.com>

--- Assets/Custom RP/Runtime/CameraRenderer.cs
+++ Assets/Custom RP/Runtime/CameraRenderer.cs
@@ -6,19 +6,52 @@ public class CameraRenderer
     private ScriptableRenderContext m_RenderContext = default;
     private Camera m_Camera = null;
 
+    private const string k_BufferName = "Render Camera";
+    private CommandBuffer m_Buffer = new() {name = k_BufferName};
+    private CullingResults m_CullingResults = default;
+
     public void Render(ScriptableRenderContext renderContext, Camera camera)
     {
         m_RenderContext = renderContext;
         m_Camera = camera;
+
+        if (!Cull()) return;
 
         Setup();
         DrawVisibleGeometry();
         Submit();
     }
 
-    private void Setup() { m_RenderContext.SetupCameraProperties(m_Camera); }
+    private void Setup()
+    {
+        m_RenderContext.SetupCameraProperties(m_Camera);
+        m_Buffer.ClearRenderTarget(true, true, Color.clear);
+
+        m_Buffer.BeginSample(k_BufferName);
+        ExecuteCommandBuffer();
+    }
 
     private void DrawVisibleGeometry() { m_RenderContext.DrawSkybox(m_Camera); }
 
-    private void Submit() { m_RenderContext.Submit(); }
+    private void Submit()
+    {
+        m_Buffer.EndSample(k_BufferName);
+        ExecuteCommandBuffer();
+
+        m_RenderContext.Submit();
+    }
+
+    private void ExecuteCommandBuffer()
+    {
+        m_RenderContext.ExecuteCommandBuffer(m_Buffer);
+        m_Buffer.Clear();
+    }
+
+    private bool Cull()
+    {
+        if (!m_Camera.TryGetCullingParameters(out ScriptableCullingParameters cullingParameters)) return false;
+
+        m_CullingResults = m_RenderContext.Cull(ref cullingParameters);
+        return true;
+    }
 }
