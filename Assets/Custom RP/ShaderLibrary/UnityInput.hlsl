a79d669cf77c09456ae2c86ab884d780454c3e39
XueJia Wang <wangxuejia@outlook.com>
2024-03-02 19:11:59 +0800

    feat: use core rp library to replace custom defined common function
    Signed-off-by: XueJia Wang <wangxuejia@outlook.com>

--- Assets/Custom RP/ShaderLibrary/UnityInput.hlsl
+++ Assets/Custom RP/ShaderLibrary/UnityInput.hlsl
@@ -1,2 +1,10 @@
 float4x4 unity_ObjectToWorld;
+float4x4 unity_WorldToObject;
+real4 unity_WorldTransformParams;
+
 float4x4 unity_MatrixVP;
+float4x4 unity_MatrixV;
+float4x4 unity_MatrixInvV;
+float4x4 unity_prev_MatrixM;
+float4x4 unity_prev_MatrixIM;
+float4x4 glstate_matrix_projection;
