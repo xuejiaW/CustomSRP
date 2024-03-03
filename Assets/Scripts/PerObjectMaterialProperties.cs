3b55079c32c1c734ec30a853e9045f49e42cc303
XueJia Wang <wangxuejia@outlook.com>
2024-03-03 21:30:34 +0800

    feat: support cutoff in PerObjectMaterialProperties
    Signed-off-by: XueJia Wang <wangxuejia@outlook.com>

--- Assets/Scripts/PerObjectMaterialProperties.cs
+++ Assets/Scripts/PerObjectMaterialProperties.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 public class PerObjectMaterialProperties : MonoBehaviour
 {
     private static int s_BaseColorId = Shader.PropertyToID("m_BaseColor");
+    private static int s_CutoffId = Shader.PropertyToID("m_Cutoff");
     private static MaterialPropertyBlock s_Block;
 
+    [SerializeField] private float m_Cutoff = 0.5f;
     [SerializeField] private Color m_BaseColor = Color.white;
 
     private void Awake() { OnValidate(); }
@@ -14,6 +16,7 @@ public class PerObjectMaterialProperties : MonoBehaviour
     {
         s_Block ??= new MaterialPropertyBlock();
         s_Block.SetColor(s_BaseColorId, m_BaseColor);
+        s_Block.SetFloat(s_CutoffId, m_Cutoff);
         GetComponent<Renderer>().SetPropertyBlock(s_Block);
     }
 }
