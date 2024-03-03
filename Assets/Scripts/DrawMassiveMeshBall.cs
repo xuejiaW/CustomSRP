18329f28fc1e763d1a84e17cd6f487a58488fa94
XueJia Wang <wangxuejia@outlook.com>
2024-03-03 14:53:23 +0800

    feat: use Graphics.DrawMeshInstanced to draw massive spheres
    Signed-off-by: XueJia Wang <wangxuejia@outlook.com>

---
+++ Assets/Scripts/DrawMassiveMeshBall.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class DrawMassiveMeshBall : MonoBehaviour
+{
+    private static int s_BaseColorId = Shader.PropertyToID("m_BaseColor");
+
+    [SerializeField] private Mesh m_Mesh = default;
+
+    [SerializeField] private Material m_Material = default;
+
+    private Matrix4x4[] m_Matrices = new Matrix4x4[1023];
+    private Vector4[] m_BaseColors = new Vector4[1023];
+    private MaterialPropertyBlock m_Block;
+
+    private void Awake()
+    {
+        for (int i = 0; i != m_Matrices.Length; ++i)
+        {
+            m_Matrices[i] = Matrix4x4.TRS(Random.insideUnitSphere * 10f, Quaternion.identity, Vector3.one);
+            m_BaseColors[i] = new Vector4(Random.value, Random.value, Random.value, 1);
+        }
+    }
+
+    private void Update()
+    {
+        if (m_Block == null)
+        {
+            m_Block = new MaterialPropertyBlock();
+            m_Block.SetVectorArray(s_BaseColorId, m_BaseColors);
+        }
+
+        Graphics.DrawMeshInstanced(m_Mesh, 0, m_Material, m_Matrices, 1023, m_Block);
+    }
+}
